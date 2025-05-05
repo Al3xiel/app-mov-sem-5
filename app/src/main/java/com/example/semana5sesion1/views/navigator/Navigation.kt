7aa7b6d2b7f65d0b40d68752e5dd47bd98787d79
Al3xiel <gabriel280104casas@gmail.com>
2025-05-04 21:59:31 -0500

    feat(3rd-step): added navigation route with embedded id.

--- app/src/main/java/com/example/semana5sesion1/views/navigator/Navigation.kt
+++ app/src/main/java/com/example/semana5sesion1/views/navigator/Navigation.kt
@@ -6,6 +6,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.semana5sesion1.viewmodel.UsuViewModel
+import com.example.semana5sesion1.views.Edit
 import com.example.semana5sesion1.views.Home
 import com.example.semana5sesion1.views.Lists
 
@@ -15,5 +16,12 @@ fun Navigation(viewModel: UsuViewModel, context: Context){
     NavHost(navController=rememberScreen, startDestination="S1"){
         composable("S1"){Home(viewModel, context, rememberScreen)}
         composable("S2"){Lists(viewModel, context, rememberScreen)}
+        composable("S3/{userId}") { backStackEntry ->
+            val userId = backStackEntry.arguments?.getString("userId")?.toIntOrNull()
+            val usuario = viewModel.userslist.find { it.id == userId }
+            if (usuario != null) {
+                Edit(viewModel, context, rememberScreen, usuario)
+            }
+        }
     }
 }
